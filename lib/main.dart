f0790de76ff3807ba7bb8ebd0b3c935181c35290
Vitor Leonardo <vitorlrc@gmail.com>
2020-10-01 17:54:39 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Text(
+      'Bem Vindo ao ByteBank as dasdasdasdas assdoaskdoiajs doija soidj aosijdo aisjd oasjd oaisjd oi ajsdo ajsdo asjd oasjdo asjd oasoijd oasjd oasijd oasijd oasjd oasdj oasdj osajd',
+      textDirection: TextDirection.ltr));
+}
